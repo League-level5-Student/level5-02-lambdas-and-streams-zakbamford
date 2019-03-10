336c3f89c593670012f534999faa7c73f8558ab5
League Student <league@iMac-9.attlocal.net>
2019-03-10 16:28:48 -0700

    CreatingStreams, StreamSorter, StreamSearcher

--- src/_02_Creating_Streams/CreatingStreams.java
+++ src/_02_Creating_Streams/CreatingStreams.java
@@ -2,6 +2,7 @@ package _02_Creating_Streams;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Stack;
 import java.util.stream.Stream;
@@ -10,23 +11,39 @@ public class CreatingStreams {
 	public static void main(String[] args) {
 		//1. Create a stream out of the following collections.
 		String[] strArr = {"one", "two", "three", "four"};
+		Stream<String> s1 = Arrays.stream(strArr);
+		s1.forEach((s) -> {
+			System.out.println(s);
+		});
 		
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		intList.add(1);
 		intList.add(2);
 		intList.add(3);
 		intList.add(4);
+		Stream<Integer> s2 = intList.stream();
+		s2.forEach((i) -> {
+			System.out.println(i);
+		});
 		
 		Stack<Random> randStack = new Stack<Random>();
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
+		Stream<Random> s3 = randStack.stream();
+		s3.forEach((r) -> {
+			System.out.println(r);
+		});
 		
 		ArrayDeque<Double> dQueue = new ArrayDeque<Double>();
 		dQueue.push(0.0);
 		dQueue.push(1.0);
 		dQueue.push(2.0);
 		dQueue.push(3.0);
+		Stream<Double> s4 = dQueue.stream();
+		s4.forEach((d) -> {
+			System.out.println(d);
+		});
 	}
 }
--- src/_04_Searching_With_Streams/StreamSearcher.java
+++ src/_04_Searching_With_Streams/StreamSearcher.java
@@ -10,12 +10,15 @@ public class StreamSearcher {
 	// use Streams to help us out.
 	public long containsCount(String[] strArr, String strToCount) {
 		//2. Convert the String array to a stream
+		Stream<String> s1 = Arrays.stream(strArr);
 		
 		//3. Use the filter method with a lambda to extract all the String
 		//   elements that match the String parameter.
+		Stream<String> s2 = s1.filter((s) -> {
+			return strToCount.equals(s);
+		});
 		
 		//4. Return the count of the filtered stream.
-		
-		return 0;
+		return s2.count();
 	}
 }
